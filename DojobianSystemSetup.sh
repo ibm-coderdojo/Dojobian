9ee600c3b04ab2aa010b4fc096d7d086558b6d14
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-01-14 19:00:35 +0000

    Update DojobianSystemSetup.sh

--- DojobianSystemSetup.sh
+++ DojobianSystemSetup.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+#######################################################################
+DojobianSystemSetup.sh
+#######################################################################
+
 /usr/bin/wget --no-check-certificate -O /root/DojobianSystemConfiguration.sh https://raw.githubusercontent.com/ibm-coderdojo/Dojobian/master/DojobianSystemConfiguration.sh
 /bin/chmod ug+x /root/DojobianSystemConfiguration.sh
 
