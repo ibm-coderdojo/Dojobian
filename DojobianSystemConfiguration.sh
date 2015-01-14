ea90b0e03a5dd4b7f57aff56732e68ca730c908d
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-01-14 16:36:26 +0000

    Update DojobianSystemConfiguration.sh

--- DojobianSystemConfiguration.sh
+++ DojobianSystemConfiguration.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-/usr/bin/wget --no-check-certificate -O /root/DojobianSystemUpdate.sh https://raw.githubusercontent.com/ibm-coderdojo/Dojobian/master/SystemUpdate.sh
+/usr/bin/wget --no-check-certificate -O /root/DojobianSystemUpdate.sh https://raw.githubusercontent.com/ibm-coderdojo/Dojobian/master/DojobianSystemUpdate.sh
 /bin/chmod ug+x /root/DojobianSystemUpdate.sh
 
 read -t 10 -p "Hit ENTER to close this window" 
