a6b4b94ef4b8488b047bbefbc6e1ad45f988189a
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-01-14 20:02:00 +0000

    Update DojobianSystemSetup.sh

--- DojobianSystemSetup.sh
+++ DojobianSystemSetup.sh
@@ -3,6 +3,23 @@
 #######################################################################
 DojobianSystemSetup.sh
 #######################################################################
+echo "
+#######################################################################
+Install tools
+#######################################################################
+"
+/usr/bin/apt-get -y install vim vim-doc vim-scripts ctags
+/usr/bin/apt-get -y install curl
+
+echo " 
+#######################################################################
+Install development tools
+#######################################################################
+"
+/usr/bin/apt-get -y install subversion subversion-tools
+/usr/bin/apt-get -y install git git-doc git-cvs git-svn git-gui
+/usr/bin/apt-get -y install eclipse*
+
 
 /usr/bin/wget --no-check-certificate -O /root/DojobianSystemConfiguration.sh https://raw.githubusercontent.com/ibm-coderdojo/Dojobian/master/DojobianSystemConfiguration.sh
 /bin/chmod ug+x /root/DojobianSystemConfiguration.sh
