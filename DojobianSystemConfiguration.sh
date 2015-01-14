c8c2bece5c112fa7faf1550bcb9d93549ef8301d
Lorenzo Cipriani <lorenzo1974@gmail.com>
2015-01-14 16:04:41 +0000

    Update and rename SystemConfiguration.sh to DojobianSystemConfiguration.sh

---
+++ DojobianSystemConfiguration.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+/usr/bin/wget --no-check-certificate -O /root/DojobianSystemUpdate.sh https://raw.githubusercontent.com/ibm-coderdojo/Dojobian/master/SystemUpdate.sh
+
+/bin/bash /root/DojobianSystemUpdate.sh
