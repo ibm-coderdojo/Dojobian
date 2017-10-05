3f09dafd8bf19b7445a001094d96ff76da743823
lorenzocipriani <lorenzo1974@gmail.com>
2017-10-05 10:53:57 +0100

    reset workspace

--- Dojobian/DojobianSystemConfiguration.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-/usr/bin/wget --no-check-certificate -O /root/DojobianSystemUpdate.sh https://raw.githubusercontent.com/ibm-coderdojo/Dojobian/master/DojobianSystemUpdate.sh
-/bin/chmod ug+x /root/DojobianSystemUpdate.sh
-
-read -t 10 -p "Hit ENTER to close this window" 
-exit 0
--- Dojobian/DojobianSystemUpdate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -e "Update the software repository"
-/usr/bin/apt-get update
-
-echo -e "Update the system"
-/usr/bin/apt-get -y upgrade
-
-read -t 10 -p "Hit ENTER to close this window" 
